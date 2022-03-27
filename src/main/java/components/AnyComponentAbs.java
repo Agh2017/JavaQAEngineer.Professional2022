b3af5ffc7f83b9688040bba33034b51a33229df5
Agh2017 <agh2017@yandex.ru>
2022-03-27 09:56:42 +0300

    feat(test): added test click_favourite_course_tile

---
+++ src/main/java/components/AnyComponentAbs.java
@@ -0,0 +1,41 @@
+package components;
+
+import actions.CommonActions;
+import annotations.Component;
+import annotations.UrlPrefix;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import waiters.StandartWaiter;
+
+public abstract class AnyComponentAbs<T> extends CommonActions<T> {
+
+  {
+    this.standartWaiter.waitForCondition(ExpectedConditions.visibilityOfElementLocated(getComponentLocator()));
+  }
+
+  protected String baseLocator;
+
+  public AnyComponentAbs(WebDriver driver) {
+    super(driver);
+  }
+
+  private By getComponentLocator() {
+    Component component = getClass().getAnnotation(Component.class);
+
+    if (component != null) {
+      String value = component.value();
+
+      baseLocator = value;
+
+      if(value.startsWith("/")) {
+        return By.xpath(value);
+      }
+      return By.cssSelector(value);
+    }
+
+    return null;
+  }
+
+}
