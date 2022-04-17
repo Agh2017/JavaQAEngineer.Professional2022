073487515ba177dd80128cb9b5f7286954d3abba
fantom <Agh2017@yandex.ru>
2022-04-17 20:25:12 +0300

    fix(findbugs) fix style

--- src/main/java/utils/DateFromCalendar.java
+++ src/main/java/utils/DateFromCalendar.java
@@ -4,17 +4,16 @@ import java.util.*;
 
 public class DateFromCalendar {
 
-    public Date DateFromCalendar(int year, int month, int day) {
-        return getDate(year, month ,day);
-    }
+  public DateFromCalendar(int year, int month, int day) {
+  }
 
-    private static int YEAR = 2022;
+  private static int YEAR = 2022;
 
-    private static Date getDate(int year, int month, int day) {
+  public static Date getDate(int year, int month, int day) {
 
-        Calendar calendar = new GregorianCalendar(year, month, day);
-        Date date = calendar.getTime();
-        System.out.println(date);
-        return date;
-    }
+    Calendar calendar = new GregorianCalendar(year, month, day);
+    Date date = calendar.getTime();
+    System.out.println(date);
+    return date;
+  }
 }
