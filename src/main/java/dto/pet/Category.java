c450186ce8e763833fd3b79c87bf5f1e8b8a493d
Agh2017 <Agh2017@yandex.ru>
2022-04-25 16:55:31 +0300

    chore(test): fix and refactor

--- src/main/java/dto/pet/Category.java
+++ src/main/java/dto/pet/Category.java
@@ -1,14 +1,11 @@
 package dto.pet;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Category{
     private String name;
     private int id;
-
-    public Category(String name, int id) {
-        this.name = name;
-        this.id = id;
-    }
 }
--- src/main/java/dto/users/User.java
+++
@@ -1,4 +0,0 @@
-package dto.users;
-
-public class User {
-}
