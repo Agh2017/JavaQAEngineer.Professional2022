79cdaac60ce65dda400f8aede562883911e34ac7
fantom <Agh2017@yandex.ru>
2022-05-19 20:48:04 +0300

    chore() refactor

---
+++ src/main/java/support/GuiceScoped.java
@@ -0,0 +1,10 @@
+package support;
+
+import io.cucumber.guice.ScenarioScoped;
+import org.openqa.selenium.WebDriver;
+
+@ScenarioScoped
+public class GuiceScoped {
+    public String browserName;
+    public WebDriver driver;
+}
