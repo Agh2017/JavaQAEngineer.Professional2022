cdf8d95d2cb6b167ac9bf1b6f568a30cce321d72
Agh2017 <Agh2017@yandex.ru>
2022-05-26 16:57:30 +0300

    chore(homework_2): refactor

--- src/main/java/com/otus/components/NavigationMenuComponent.java
+++ src/main/java/com/otus/components/NavigationMenuComponent.java
@@ -7,7 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import com.otus.pages.CategoryLessonsPage;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import support.GuiceScoped;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class NavigationMenuComponent extends BaseComponent<NavigationMenuComponent> {
 
@@ -17,7 +20,7 @@ public class NavigationMenuComponent extends BaseComponent<NavigationMenuCompone
   @FindBy(xpath = "//p[text()='Курсы']") //проверить пред версию
   private WebElement navMenuCourses;
 
-  @FindBy(xpath = "//p[text()='Курсы']/../../")
+  @FindBy(xpath = "div.header2-menu__full > div:nth-child(2)") //p[text()='Курсы']/.."
   private WebElement selectCoursesInDrawer;
 
   @FindBy(css = ".header2__burger.js-open-drawer")
@@ -41,19 +44,38 @@ public class NavigationMenuComponent extends BaseComponent<NavigationMenuCompone
 
   public TrainingCoursesPage openTrainingCoursesPageFromNavigationNenu() {
     Actions actions = new Actions(guiceScoped.driver);
+
+
+    //для стандартного окна браузера (не max)
     if (drawerButton.isEnabled()) {
       drawerButton.click();
 
     }
-//    actions.moveToElement(navMenuCourses)
-//            .moveToElement(dropdownTrainingCourse)
-//            .click().build().perform();
-
-
     assert guiceScoped.driver.findElement(By.xpath("//a[@href='/online/'][@title='Подготовительные курсы']")).isEnabled();
 
-    if (selectCoursesInDrawer.isEnabled()) selectCoursesInDrawer.click();
-    if (trainingCourseSelectInDrawer.isEnabled()) trainingCourseSelectInDrawer.click(); //TODO №1  найти интерактивный серлектор
+    //
+    WebDriverWait wait = new WebDriverWait(guiceScoped.driver, 10);
+
+    for (int second = 0;; second++) {
+      if (second >= 30) fail("timeout");
+      try {
+        if (selectCoursesInDrawer.isEnabled()&& selectCoursesInDrawer.isDisplayed()) //.ic.new-ic-logo-with-text-black
+          selectCoursesInDrawer.click();
+          System.out.println("click on selectCoursesInDrawer");
+        break;
+      } catch (Exception e) {}
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+    // для полноэкранного max
+    //    actions.moveToElement(navMenuCourses)
+    //            .moveToElement(dropdownTrainingCourse)
+    //            .click().build().perform();
+
     return new TrainingCoursesPage(guiceScoped);
     //TODO delete
     // найти и кликнуть (selector = div.header2-menu.header2-menu_main > div:nth-child(1)  +++ hover),
