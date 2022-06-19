c4464f81b6ebd69c95057718fa5db0ac21088a43
Laptop <Agh2017@yandex.ru>
2022-06-20 00:59:47 +0300

    fix:(test) Fixed getNameOfCurrency soap test

--- src/test/java/testsHelpers/TestHelperSOAP.java
+++ src/test/java/testsHelpers/TestHelperSOAP.java
@@ -7,6 +7,7 @@ import com.consol.citrus.ws.message.SoapMessage;
 import features.PojoToXML;
 import org.oorsprong.websamples.CurrencyName;
 import org.oorsprong.websamples.CurrencyNameResponse;
+import org.springframework.core.io.ClassPathResource;
 import org.testng.annotations.Test;
 import com.dataaccess.webservicesserver.NumberToDollars;
 import com.dataaccess.webservicesserver.NumberToDollarsResponse;
@@ -16,43 +17,6 @@ import java.math.BigDecimal;
 public class TestHelperSOAP extends TestNGCitrusTestRunner {
 
     private TestContext context;
-
-    @Test(description = "Получение", enabled = true)
-    @CitrusTest
-    public void getTestActions() {
-        this.context = citrus.createTestContext();
-
-        PojoToXML<Class<NumberToDollars>> convRequest = new PojoToXML<>();
-        PojoToXML<Class<NumberToDollarsResponse>> convResponse = new PojoToXML<>();
-
-
-        soap(soapActionBuilder -> soapActionBuilder
-                        .client("soapHelperClient")
-                        .send()
-//                .payload(getNumberToDollarsRequest(), "marshallerRequest")
-                        .payload(convRequest.convert(NumberToDollars.class, getNumberToDollarsRequest(), "http://www.dataaccess.com/webservicesserver/", "NumberToDollars"))
-        );
-
-        soap(soapActionBuilder -> soapActionBuilder
-                        .client("soapHelperClient")
-                        .receive()
-                        .xsdSchemaRepository("schemaRepositoryService")
-//                .payload(getNumberToDollarsResponse(), "marshallerResponse")
-                        .payload(convResponse.convert(NumberToDollarsResponse.class, getNumberToDollarsResponse(), "http://www.dataaccess.com/webservicesserver/", "NumberToDollarsResponse"))
-        );
-    }
-
-    public NumberToDollars getNumberToDollarsRequest() {
-        NumberToDollars numberToDollars = new NumberToDollars();
-        numberToDollars.setDNum(new BigDecimal("15"));
-        return numberToDollars;
-    }
-
-    public NumberToDollarsResponse getNumberToDollarsResponse() {
-        NumberToDollarsResponse numberToDollarsResponse = new NumberToDollarsResponse();
-        numberToDollarsResponse.setNumberToDollarsResult("fifteen dollars");
-        return numberToDollarsResponse;
-    }
 
     @Test(description = "Получение названия валюты")
     @CitrusTest
@@ -66,28 +30,29 @@ public class TestHelperSOAP extends TestNGCitrusTestRunner {
         soap(soapActionBuilder -> soapActionBuilder
                 .client("soapHelperCountryInfoClient")
                 .send()
-                .payload(convRequest.convert(CurrencyName.class, getCurrencyNameRequest(), "http://www.oorsprong.org/", "websamples.countryinfo"))
+                .payload(convRequest.convert(CurrencyName.class,
+                        getCurrencyNameRequest(), "http://www.oorsprong.org/websamples.countryinfo", "CurrencyName"))
         );
-        // продолжить на 0,57
+
         soap(soapActionBuilder -> soapActionBuilder
                 .client("soapHelperCountryInfoClient")
                 .receive()
-                .xsdSchemaRepository("schemaRepositoryService2")
-                .payload(convResponse.convert(CurrencyNameResponse.class, getCurrencyNameResponse(), "http://www.oorsprong.org/", "websamples.countryinfo"))
-        );
+                .payload(new ClassPathResource("wsdl/schemaCountryInfo.xml"))
+                .timeout(5000L));
     }
+
 
     //готовим pojo для запроса
     public CurrencyName getCurrencyNameRequest() {
         CurrencyName currencyName = new CurrencyName();
-        currencyName.setSCurrencyISOCode("DZD");
+        currencyName.setSCurrencyISOCode("RUB");
         return currencyName;
     }
 
-    //готовим эталон для валидации ответа
+    //готовим эталон для валидации ответа если есть wsdl или xsd file
     public CurrencyNameResponse getCurrencyNameResponse() {
         CurrencyNameResponse currencyNameResponse = new CurrencyNameResponse();
-        currencyNameResponse.setCurrencyNameResult("Algeria Dinars");
+        currencyNameResponse.setCurrencyNameResult("Rubles");
         return currencyNameResponse;
     }
 }
