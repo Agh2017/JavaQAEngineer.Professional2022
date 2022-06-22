5252cf309001e43785abe5a1ec47bc5d8503a53d
Agh2017 <Agh2017@yandex.ru>
2022-06-22 08:29:24 +0300

    chore: Refactoring tests

--- src/test/java/services/SpringService_Test.java
+++ src/test/java/services/SpringService_Test.java
@@ -2,9 +2,11 @@ package services;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import org.apache.http.HttpConnection;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -52,14 +54,10 @@ public class SpringService_Test {
   @Test
   public void list_courses_path_via_stub_wiremock() throws IOException {
     new AllCoursesStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    HttpGet request = new HttpGet("http://localhost:8089/course/get/all");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String responseString = getResponseString("http://localhost:8089/course/get/all");
 
     verify(getRequestedFor(urlEqualTo("/course/get/all")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(LIST_COURSES_MOCK);
   }
 
@@ -67,79 +65,47 @@ public class SpringService_Test {
   @Test
   public void users_score_path_via_stub_wiremock() throws IOException {
     new ScoreStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    HttpGet request = new HttpGet("http://localhost:8089/user/get/00001");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String responseString = getResponseString("http://localhost:8089/user/get/00001");
 
     verify(getRequestedFor(urlEqualTo("/user/get/00001")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(USER_SCORE_MOCK_ID00001);
 
-
-    request = new HttpGet("http://localhost:8089/user/get/00002");
-    httpResponse = httpClient.execute(request);
-    responseString = convertResponseToString(httpResponse);
+    responseString = getResponseString("http://localhost:8089/user/get/00002");
 
     verify(getRequestedFor(urlEqualTo("/user/get/00002")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(USER_SCORE_MOCK_ID00002);
 
   }
 
+  //clean test -Dtest=SpringService_Test#list_users_path_via_stub_wiremock
   @Test
   public void list_users_path_via_stub_wiremock() throws IOException {
     new AllUsersStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    HttpGet request = new HttpGet("http://localhost:8089/user/get/all");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String responseString = getResponseString("http://localhost:8089/user/get/all");
 
     verify(getRequestedFor(urlEqualTo("/user/get/all")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(LIST_USERS_MOCK);
   }
 
-  @Test
-  public void hello_path_via_stub_wiremock() throws IOException {
-    //создаем mock end-points
-    new UserStub();
+  private String getResponseString(String url) throws IOException {
     CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    //отправляем запрос на заглушку
-    HttpGet request = new HttpGet("http://localhost:8089/hello");
+    HttpGet request = new HttpGet(url);
     HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
 
-    //для всех GET запросов на hello мы сверяем ответ и статус
-    verify(getRequestedFor(urlEqualTo("/hello")));
-    assertThat(responseString).isEqualTo("Hello");
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
+    assertTrue("response status code error, url:" + url, (httpResponse.getStatusLine().getStatusCode() == 200));
+    return convertResponseToString(httpResponse);
   }
-
-  //реальный запрос в приложение demo (оно д.б. запущено)
-  //тест запускать через строку мавен/wiremock Run, чтобы получить урл из пропертей: clean test -Dtest=SpringService_Test#hello_path_via_real
-  @Test
-  public void hello_path_via_real() throws IOException {
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    HttpGet request = new HttpGet(String.format("%s/hello", System.getProperty("real.service.hostname")));
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
-
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
-    assertThat(responseString).isEqualTo("Hello User");
-  }
-
 
   private String convertResponseToString(HttpResponse response) throws IOException {
     InputStream responseStream = response.getEntity().getContent();
     Scanner scanner = new Scanner(responseStream, "UTF-8");
     String responseString = scanner.useDelimiter("\\Z").next();
+
     scanner.close();
     responseStream.close();
+
     return responseString;
   }
 
