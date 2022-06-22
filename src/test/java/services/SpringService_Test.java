062bc7eb4d460fbc8500a1601ed04486dfe40071
Laptop <Agh2017@yandex.ru>
2022-06-22 20:56:39 +0300

    chore: Refactoring mock tests

--- src/test/java/services/SpringService_Test.java
+++ src/test/java/services/SpringService_Test.java
@@ -1,7 +1,10 @@
 package services;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.apache.http.impl.client.HttpClients.createMinimal;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
@@ -31,103 +34,73 @@ public class SpringService_Test {
           "[{ \"name\":\"QA java\", \"price\":15000 }, { \"name\":\"Java\", \"price\":12000 }]";
   private static final String USER_SCORE_MOCK_ID00001 = "{ \"name\":\"Test user Petroff\", \"score\":78 }";
   private static final String USER_SCORE_MOCK_ID00002 = "{ \"name\":\"Test user Ivanov\", \"score\":44 }";
+  private static final String LOCALHOST_FOR_MOCK = System.getProperty("wiremock.hostname");
 
   private static WireMockServer wireMockServer;
 
   @BeforeAll
   public static void startWireMock() {
-    wireMockServer = new WireMockServer(new WireMockConfiguration().port(8080));
+    wireMockServer = new WireMockServer();
+    //new WireMockConfiguration().port(8080)
     wireMockServer.start();
   }
-
-  //clean test -Dtest=SpringService_Test#list_courses_path_via_stub_wiremock
   @Test
   public void list_courses_path_via_stub_wiremock() throws IOException {
     new AllCoursesStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    HttpGet request = new HttpGet("http://localhost:8080/course/get/all");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String url = String.format("%s/course/get/all", LOCALHOST_FOR_MOCK);
+    String responseString = responseString(url);
 
     verify(getRequestedFor(urlEqualTo("/course/get/all")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(LIST_COURSES_MOCK);
   }
 
-  //clean test -Dtest=SpringService_Test#users_score_path_via_stub_wiremock
   @Test
   public void users_score_path_via_stub_wiremock() throws IOException {
     new ScoreStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    HttpGet request = new HttpGet("http://localhost:8089/user/get/00001");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String url = String.format("%s/user/get/00001", LOCALHOST_FOR_MOCK);
+    String responseString = responseString(url);
+
 
     verify(getRequestedFor(urlEqualTo("/user/get/00001")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(USER_SCORE_MOCK_ID00001);
 
-
-    request = new HttpGet("http://localhost:8089/user/get/00002");
-    httpResponse = httpClient.execute(request);
-    responseString = convertResponseToString(httpResponse);
+    url = String.format("%s/user/get/00002", LOCALHOST_FOR_MOCK);
+    responseString = responseString(url);
 
     verify(getRequestedFor(urlEqualTo("/user/get/00002")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(USER_SCORE_MOCK_ID00002);
+
   }
 
   @Test
   public void list_users_path_via_stub_wiremock() throws IOException {
     new AllUsersStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
 
-    HttpGet request = new HttpGet("http://localhost:8089/user/get/all");
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
+    String url = String.format("%s/user/get/all", LOCALHOST_FOR_MOCK);
+    String responseString = responseString(url);
 
     verify(getRequestedFor(urlEqualTo("/user/get/all")));
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
     assertThat(responseString).isEqualTo(LIST_USERS_MOCK);
+
   }
 
-  @Test
-  public void hello_path_via_stub_wiremock() throws IOException {
-    //создаем mock end-points
-    new UserStub();
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    //отправляем запрос на заглушку
-    HttpGet request = new HttpGet("http://localhost:8089/hello");
+  private String responseString(String url) throws IOException {
+    CloseableHttpClient httpClient = createMinimal();
+    HttpGet request = new HttpGet(url);
     HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
 
-    //для всех GET запросов на hello мы сверяем ответ и статус
-    verify(getRequestedFor(urlEqualTo("/hello")));
-    assertThat(responseString).isEqualTo("Hello");
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
-  }
+    assertEquals(200, httpResponse.getStatusLine().getStatusCode(), "Server response error");
 
-  //реальный запрос в приложение demo (оно д.б. запущено)
-  //тест запускать через строку мавен/wiremock Run, чтобы получить урл из пропертей: clean test -Dtest=SpringService_Test#hello_path_via_real
-  @Test
-  public void hello_path_via_real() throws IOException {
-    CloseableHttpClient httpClient = HttpClients.createDefault();
-
-    HttpGet request = new HttpGet(String.format("%s/hello", System.getProperty("real.service.hostname")));
-    HttpResponse httpResponse = httpClient.execute(request);
-    String responseString = convertResponseToString(httpResponse);
-
-    assertThat(httpResponse.getStatusLine().getStatusCode()).isEqualTo(200);
-    assertThat(responseString).isEqualTo("Hello User");
+    return convertResponseToString(httpResponse);
   }
 
   private String convertResponseToString(HttpResponse response) throws IOException {
     InputStream responseStream = response.getEntity().getContent();
     Scanner scanner = new Scanner(responseStream, "UTF-8");
     String responseString = scanner.useDelimiter("\\Z").next();
+
     scanner.close();
     responseStream.close();
     return responseString;
